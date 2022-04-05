6e889c1f1a620d9a5ef16edbce4f7a390ab77962
ycj1379 <ycj1379@gmail.com>
2022-04-05 13:00:24 +0800

    Add files via upload

--- start.sh
+++ start.sh
@@ -10,8 +10,8 @@ StoreFiles=https://github.com/ycj1379/railway-self/raw/main/etc/StoreFiles
 #PORT=4433
 mkdir -p /etc/caddy/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
 wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
-wget -qO- $CONFIGCADDY | sed -e "1c :$PORT" -e "s/\$AUUID/$AUUID/g" -e "s/\$MYUUID-HASH/$(caddy hash-password --plaintext $AUUID)/g" >/etc/caddy/Caddyfile
-wget -qO- $CONFIGRAY | sed -e "s/\$AUUID/$AUUID/g" -e "s/\$ParameterSSENCYPT/$ParameterSSENCYPT/g" >/ray.json
+wget -qO- $CONFIGCADDY  /etc/caddy/Caddyfile
+wget -qO- $CONFIGRAY  /ray.json
 
 # storefiles
 mkdir -p /usr/share/caddy/$AUUID && wget -O /usr/share/caddy/$AUUID/StoreFiles $StoreFiles
